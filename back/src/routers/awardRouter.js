c33c4ea675db8bcf9220b6384c52e83beb48f5b4
@eikjae <eikjae@gmail.com>
2022-03-16 03:57:05 +0900

    Award MVP router 코드 작성 완료

---
+++ back/src/routers/awardRouter.js
@@ -0,0 +1,38 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { awardAuthService } from "../services/awardService";
+
+const awardAuthRouter = Router();
+
+awardAuthRouter.post("/award/create", async function (req, res, next) {
+  
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    // req (request)에서 데이터 가져오기
+    const user_id = req.body.user_id;
+    const title = req.body.title;
+    const description = req.body.description;
+
+    // 위 데이터를 유저 db에 추가하기
+    const newAward = await awardAuthService.addAward({
+      user_id,
+      title,
+      description,
+    });
+
+    if (newAward.errorMessage) {
+      throw new Error(newAward.errorMessage);
+    }
+
+    res.status(201).json(newAward);
+  } catch (err) {
+    next(err);
+  }  
+});
+
+export { awardAuthRouter };
