e4e3d182dff9216fcde5ee720002fe3b43757990
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-16 21:01:18 +0900

    /educations/:id 작성 완료

--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -23,6 +23,52 @@ class userEducationService {
 
     return createdNewEducation;
   }
+
+  static async getUserEducation({ _id }) {
+    const user = await Education.findById({ _id });
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!user) {
+      const errorMessage =
+        "학력 정보를 추가하지 않았습니다.";
+      return { errorMessage };
+    }
+
+    return user;
+  }
+
+  static async setEducation({ _id, toUpdate }) {
+    // 우선 해당 id의 학력이 db에 존재하는지 여부 확인
+    let education = await Education.findById({ _id });
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!education) {
+      const errorMessage =
+        "존재하지 않는 학력 정보입니다.";
+      return { errorMessage };
+    }
+
+    // 업데이트 대상에 school 있다면, 즉 school 값이 null 이 아니라면 업데이트 진행
+    if (toUpdate.school) {
+      const fieldToUpdate = "school";
+      const newValue = toUpdate.school;
+      education = await Education.update({ _id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.major) {
+      const fieldToUpdate = "major";
+      const newValue = toUpdate.major;
+      education = await Education.update({ _id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.position) {
+      const fieldToUpdate = "position";
+      const newValue = toUpdate.position;
+      education = await Education.update({ _id, fieldToUpdate, newValue });
+    }
+
+    return education;
+  }
 }
 
 export { userEducationService };
