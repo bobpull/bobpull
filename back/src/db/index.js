bd24cc63aaafac1391f875f9199125d836805e78
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-16 19:48:52 +0900

    /education/create 완료

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
+import { Education } from "./models/Education";
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -15,4 +16,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User };
+export { User, Education };
---
+++ back/src/db/models/Education.js
@@ -0,0 +1,15 @@
+import { EducationModel } from "../schemas/education";
+
+class Education {
+  static async create({ newEducation }) {
+    const createdNewEducation = await EducationModel.create(newEducation);
+    return createdNewEducation;
+  }
+
+  static async findBySchool({ school, major, position }) {
+    const schoolMajorPosition = await EducationModel.findOne({ school, major, position });
+    return schoolMajorPosition;
+  }
+}
+
+export { Education };
