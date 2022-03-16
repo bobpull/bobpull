9cbbc8ae7d42bb4ff41d2c1c794945824dc651a2
jjssong <wkdthdrnt@naver.com>
2022-03-16 16:00:05 +0900

    데이터베이스와 연결 전 로컬 변수에 프로젝트 정보를 저장하고 화면에 나오게 하기

--- front/src/components/project/Project.js
+++ front/src/components/project/Project.js
@@ -15,19 +15,53 @@ const Title = styled.h2`
   font-weight: normal;
   font-size: 1.25rem;
 `
+const ProjectList = styled.article`
+  text-align: left;
+`
+
+const ProjectTitle = styled.h5`
+  font-weight: normal;
+  font-size: 1.25rem;
+  line-height: 1.2;
+  margin: 0;
+  
+`
+
+const ProjectDes = styled.p`
+  margin: 0;
+  color: #6c757d;
+`
+
+const ProjectDate = styled.p`
+color: #6c757d;
+`
+
 
 const Project = () => {
-  const [click, setClick] = useState(false)
-  
+  const [isEditing, setIsEditing] = useState(false)
+  const [projects, setProjects] = useState([])
   return (
     <ProjectContainer>
       <Title>Project</Title>
+      {projects && projects.map(item => (
+        <ProjectList>
+          <ProjectTitle>{item.name}</ProjectTitle>
+          <ProjectDes>{item.description}</ProjectDes>
+          <ProjectDate>{item.startDate} ~ {item.dueDate}</ProjectDate>
+        </ProjectList>
+      ))}
       <Button
-        onClick={() => setClick(true)}
+        onClick={() => setIsEditing(true)}
         variant="primary" 
         className="mb-3"
       >추가</Button>
-      {click && <AddProject/>}
+      {isEditing &&
+       <AddProject
+        setIsEditing={setIsEditing}
+        projects={projects}
+        setProjects={setProjects}
+      />
+       }
     </ProjectContainer>
   );
 }
