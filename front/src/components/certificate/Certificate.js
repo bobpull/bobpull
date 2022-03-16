061899e4374cad2eeb8c46d605e7c094ba88ce42
eunna <dladmssk98@gmail.com>
2022-03-16 20:29:35 +0900

    Certificate API 명세에 따라 변수명 수정

--- front/src/components/certificate/Certificate.js
+++ front/src/components/certificate/Certificate.js
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import CertificateEditForm from "./CertificateEditForm";
+import CertificateCard from "./CertificateCard";
+import * as Api from "../../api";
 
-function Certificate() {
+function Certificate({ portfolioOwnerId }) {
+  const [isEditing, setIsEditing] = useState(false);
   const [addCertificate, setAddCertificate] = useState(false);
+  const [certificates, setCertificates] = useState([]);
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // "users/유저id" 엔드포인트로 GET 요청을 하고, user를 response의 data로 세팅함.
+    Api.get("users", portfolioOwnerId).then((res) => setUser(res.data));
+    Api.get("certificatelist", portfolioOwnerId).then((res) =>
+      setCertificates(res.data)
+    );
+  }, [portfolioOwnerId]);
+
   return (
     <Card className="ml-2">
       <Card.Body>
         <Card.Title>자격증</Card.Title>
+
+        {certificates.map((v) => (
+          <CertificateCard certificate={v} setIsEditing={setIsEditing} />
+        ))}
+
         <Button
           onClick={() => {
             setAddCertificate(true);
@@ -16,7 +36,13 @@ function Certificate() {
           +
         </Button>
 
-        {addCertificate && <CertificateEditForm />}
+        {addCertificate && (
+          <CertificateEditForm
+            user={user}
+            setAddCertificate={setAddCertificate}
+            setIsEditing={setIsEditing}
+          />
+        )}
       </Card.Body>
     </Card>
   );
--- front/src/components/certificate/CertificateEditForm.js
+++ front/src/components/certificate/CertificateEditForm.js
@@ -1,36 +1,75 @@
+import { useState } from "react";
 import { Container, Col, Row, Form, Button } from "react-bootstrap";
+import * as Api from "../../api";
 
-function CertificateEditForm() {
-  const handleSubmit = () => {};
+function CertificateEditForm({ user, setIsEditing }) {
+  // useState로 title 상태를 생성함.
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // "certificate/create" 엔드포인트로 POST 요청함.
+      const res = await Api.post("certificate/create", {
+        user_id: user.id,
+        title: title,
+        description: description,
+        when_date: date,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    setIsEditing(false);
+  };
 
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col>
           <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="certificateName">
+            <Form.Group controlId="certificateEditTitle">
               <Form.Control
                 type="text"
                 placeholder="자격증 제목"
                 className="mt-3"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               ></Form.Control>
             </Form.Group>
 
-            <Form.Group controlId="certificateDetail">
+            <Form.Group controlId="certificateEditDetail">
               <Form.Control
                 type="text"
                 placeholder="상세내역"
                 className="mt-3"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               ></Form.Control>
             </Form.Group>
 
-            <Form.Group controlId="certificateDate">
+            <Form.Group controlId="certificateEditDate">
               <Form.Control
                 type="date"
                 className="mt-3"
                 formatter="yyyy/MM/dd"
-                value={Date.now()}
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
               ></Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mt-3 text-center">
+              <Col sm={{ span: 20 }}>
+                <Button variant="primary" type="submit" className="me-3">
+                  확인
+                </Button>
+                <Button variant="secondary" onClick={() => setIsEditing(false)}>
+                  취소
+                </Button>
+              </Col>
             </Form.Group>
           </Form>
         </Col>
