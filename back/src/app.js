51d201e12c576e0db55c0cc4bd51d32b735e56c9
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-16 16:32:16 +0900

    userProjectRouter 추가

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import { userAuthRouter } from "./routers/userRouter";
+import { userProjectRouter } from "./routers/projectRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
+app.use(userProjectRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
