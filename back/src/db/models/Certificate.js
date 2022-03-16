8a2324e43dbfd811a1a33fd7fab86a1ac610966d
@eikjae <eikjae@gmail.com>
2022-03-16 19:13:34 +0900

    Certificate MVP (PUT, certificates/:id) model 완성

--- back/src/db/models/Certificate.js
+++ back/src/db/models/Certificate.js
@@ -5,6 +5,23 @@ class Certificate {
     const createdNewCertificate = await CertificateModel.create(newCertificate);
     return createdNewCertificate;
   }
-}
 
+  static async findById({ certificate_id }) {
+    const certificate = await CertificateModel.findOne({ id: certificate_id });
+    return certificate;
+  }
+
+  static async update({ certificate_id, fieldToUpdate, newValue }) {
+    const filter = { id: certificate_id };
+    const update = { [fieldToUpdate]: newValue };
+    const option = { returnOriginal: false };
+
+    const updatedCertificate = await CertificateModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return updatedCertificate;
+  }
+}
 export { Certificate };
