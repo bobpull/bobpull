d0024e2997d3dbad2bd9d0423d1c40b0ebf066d0
@eikjae <eikjae@gmail.com>
2022-03-16 17:59:45 +0900

    Certificate MVP (POST, certificate/create) model 완성

---
+++ back/src/db/models/Certificate.js
@@ -0,0 +1,10 @@
+import { CertificateModel } from "../schemas/certificate";
+
+class Certificate {
+  static async create({ newCertificate }) {
+    const createdNewCertificate = await CertificateModel.create(newCertificate);
+    return createdNewCertificate;
+  }
+}
+
+export { Certificate };
