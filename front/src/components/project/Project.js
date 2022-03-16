b481e850d849fa16b9439a8dfd2209ff04407408
jjssong <wkdthdrnt@naver.com>
2022-03-17 02:47:57 +0900

    get요청 적용

--- front/src/components/project/Project.js
+++ front/src/components/project/Project.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios"
+
 import AddProject from "./AddProject"
 import ProjectList from "./ProjectList"
+import ProjectEditForm from "./ProjectEditForm"
 
 import styled from "styled-components"
 import {Button} from "react-bootstrap";
@@ -16,32 +18,53 @@ const Title = styled.h2`
   font-weight: normal;
   font-size: 1.25rem;
 `
+const ListContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+`
+
 const Project = () => {
   const [isEditing, setIsEditing] = useState(false)
   const [projects, setProjects] = useState([])
+
+  // get projects data
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const res = await axios.get('')
+      setProjects(res.data.data)
+    }
+    fetchAPI()
+  }, [])
+
   return (
     <ProjectContainer>
       <Title>Project</Title>
       {projects && projects.map(item => 
-      <>
+        <ListContainer>
           <ProjectList
             project={item}
           />
-        </>
+          <Button 
+            variant="outline-info" 
+            size="sm"
+            onClick={() => {}}
+          >편집</Button>
+        </ListContainer>
         )}
+      
       <Button
         onClick={() => setIsEditing(true)}
         variant="primary"
         className="mb-3"
       >추가</Button>
       
-      {isEditing &&
-       <AddProject
+       {isEditing && <AddProject
         setIsEditing={setIsEditing}
         projects={projects}
         setProjects={setProjects}
-      />
-       }
+      />}
+       
     </ProjectContainer>
   );
 }
