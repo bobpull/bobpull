efcc6be736b9b8c1491084e2b604751c2767f1af
eunna <dladmssk98@gmail.com>
2022-03-16 20:39:00 +0900

    Certificate 컴포넌트에 props로 ownerId 전달

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -64,7 +64,7 @@ function Portfolio() {
         <Col>
           <div style={{ textAlign: "center" }}>
             학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
-            <Certificate />
+            <Certificate portfolioOwnerId={portfolioOwner.id} />
           </div>
         </Col>
       </Row>
