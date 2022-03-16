e47bed631cbf0c11d2b78a472e443104794d7aca
@eikjae <eikjae@gmail.com>
2022-03-16 18:01:31 +0900

    Certificate MVP 추가를 위한 간단한 코드 추가

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
 import { Award } from "./models/Award";
+import { Certificate } from "./models/Certificate";
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -16,4 +17,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Award };
+export { User, Award, Certificate };
