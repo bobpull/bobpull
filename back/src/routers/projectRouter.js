89a9e7b55e0f131c8ebffa7bb9a2cf2aa6291c62
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-16 17:06:05 +0900

    /projects/:id 수정 구현 완료

--- back/src/routers/projectRouter.js
+++ back/src/routers/projectRouter.js
@@ -60,6 +60,34 @@ userProjectRouter.get(
   }
 );
 
+userProjectRouter.put(
+  "/projects/:id",
+  async function (req, res, next) {
+    try {
+      // URI로부터 프로젝트 id를 추출함.
+      const id = req.params.id;
+      // body data 로부터 업데이트할 프로젝트 정보를 추출함.
+      const title = req.body.title ?? null;
+      const description = req.body.description ?? null;
+      const from_date = req.body.from_date ?? null;
+      const to_date = req.body.to_date ?? null;
+  
+      const toUpdate = { title, description, from_date, to_date };
+  
+      // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
+      const updatedProject = await userProjectService.setProject({ id, toUpdate });
+  
+      if (updatedProject.errorMessage) {
+        throw new Error(updatedProject.errorMessage);
+      }
+  
+      res.status(200).json(updatedProject);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 userProjectRouter.get(
   "/projectlist/:user_id",
   async function (req, res, next) {
--- back/src/services/projectService.js
+++ back/src/services/projectService.js
@@ -23,6 +23,45 @@ class userProjectService {
     return createdNewProject;
   }
 
+  static async setProject({ id, toUpdate }) {
+    // 우선 해당 id 의 유저가 db에 존재하는지 여부 확인
+    let project = await Project.findById({ id });
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!project) {
+      const errorMessage =
+        "프로젝트가 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    // 업데이트 대상에 name이 있다면, 즉 name 값이 null 이 아니라면 업데이트 진행
+    if (toUpdate.title) {
+      const fieldToUpdate = "title";
+      const newValue = toUpdate.title;
+      project = await Project.update({ id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.description) {
+      const fieldToUpdate = "description";
+      const newValue = toUpdate.description;
+      project = await Project.update({ id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.from_date) {
+      const fieldToUpdate = "from_date";
+      const newValue = toUpdate.from_date;
+      project = await Project.update({ id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.to_date) {
+      const fieldToUpdate = "to_date";
+      const newValue = toUpdate.to_date;
+      project = await Project.update({ id, fieldToUpdate, newValue });
+    }
+
+    return project;
+  }
+
   static async getUserAllProject({ user_id }) {
     const project = await Project.findAll({ user_id });
 
