5f8ab21204c10d12002a15ef4b475370e921f63b
@eikjae <eikjae@gmail.com>
2022-03-16 14:46:42 +0900

    Award MVP (PUT, awards/:id) service 완성

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -21,8 +21,35 @@ class awardAuthService {
     // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!award) {
       const errorMessage =
-        "수상이력이 없습니다. 다시 한 번 확인해 주세요.";
+        "수상 이력이 없습니다. 다시 한 번 확인해 주세요.";
         return { errorMessage };
+    }
+
+    return award;
+  }
+
+  static async setAward({ award_id, toUpdate }) {
+    // 우선 해당 id의 award가 db에 존재하는지 여부 확인
+    let award = await Award.findById({ award_id });
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!award) {
+      const errorMessage =
+        "수상 이력이 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    // 업데이트 대상에 title이 있다면, 즉 title 값이 null 이 아니라면 업데이트 진행
+    if (toUpdate.title) {
+      const fieldToUpdate = "title";
+      const newValue = toUpdate.title;
+      award = await Award.update({ award_id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.description) {
+      const fieldToUpdate = "description";
+      const newValue = toUpdate.description;
+      award = await Award.update({ award_id, fieldToUpdate, newValue });
     }
 
     return award;
