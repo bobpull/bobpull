810b842501b4ea8f4c625b309b98976517115eee
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-16 17:49:28 +0900

    add educationRouter

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 import { userAuthRouter } from "./routers/userRouter";
+import { userProjectRouter } from "./routers/projectRouter";
+import { userEducationRouter } from "./routers/educationRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
@@ -21,6 +23,8 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
+app.use(userProjectRouter);
+app.use(userEducationRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
