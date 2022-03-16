4e0d0b8f92a558a75bedbae165cbb73f2bd5d2e7
eunna <dladmssk98@gmail.com>
2022-03-16 18:16:12 +0900

    Portfolio에 Certificate 컴포넌트 추가

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -5,6 +5,7 @@ import { Container, Col, Row } from "react-bootstrap";
 import { UserStateContext } from "../App";
 import * as Api from "../api";
 import User from "./user/User";
+import Certificate from "./certificate/Certificate";
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -61,11 +62,10 @@ function Portfolio() {
           />
         </Col>
         <Col>
-
           <div style={{ textAlign: "center" }}>
             학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
+            <Certificate />
           </div>
-
         </Col>
       </Row>
     </Container>
