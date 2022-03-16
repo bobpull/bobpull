911ae5e963e8682177d138e69193efe17649543b
jjssong <wkdthdrnt@naver.com>
2022-03-16 14:19:56 +0900

    고양이와 프로젝트 레이아웃이 화면을 줄였을 때 겹치는 현상을 수정함

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -5,6 +5,7 @@ import { Container, Col, Row } from "react-bootstrap";
 import { UserStateContext } from "../App";
 import * as Api from "../api";
 import User from "./user/User";
+import Project from "./project/Project"
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -54,18 +55,16 @@ function Portfolio() {
   return (
     <Container fluid>
       <Row>
-        <Col md="3" lg="3">
+        <Col lg="3">
           <User
             portfolioOwnerId={portfolioOwner.id}
             isEditable={portfolioOwner.id === userState.user?.id}
           />
         </Col>
         <Col>
-
           <div style={{ textAlign: "center" }}>
-            학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
+            <Project/>
           </div>
-
         </Col>
       </Row>
     </Container>
