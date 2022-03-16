3e8ddc376076cb6c53fd32d29d888edd560594bf
jjssong <wkdthdrnt@naver.com>
2022-03-16 18:06:22 +0900

    입력받은 프로젝트 정보를 처리하는 함수 백엔드 api가 개발되면 로직 수정

---
+++ front/src/components/project/ProjectList.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+const ProjectList1 = styled.article`
+  text-align: left;
+`
+
+const ProjectTitle = styled.h5`
+  font-weight: normal;
+  font-size: 1.25rem;
+  line-height: 1.2;
+  margin: 0;
+  
+`
+
+const ProjectDes = styled.p`
+  margin: 0;
+  color: #6c757d;
+`
+
+const ProjectDate = styled.p`
+color: #6c757d;
+`
+
+const ProjectList = ({project}) => {
+  return (
+    <ProjectList1>
+      <ProjectTitle>{project.name}</ProjectTitle>
+      <ProjectDes>{project.description}</ProjectDes>
+      <ProjectDate>{project.startDate} ~ {project.dueDate}</ProjectDate>
+    </ProjectList1>   
+  )
+}
+
+export default ProjectList
