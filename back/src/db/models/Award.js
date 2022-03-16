b617a30bea0d9976f42b951d518e9341bbefd7e9
@eikjae <eikjae@gmail.com>
2022-03-16 14:47:43 +0900

    Award MVP (PUT, awards/:id) model 완성

--- back/src/db/models/Award.js
+++ back/src/db/models/Award.js
@@ -10,6 +10,19 @@ class Award {
     const award = await AwardModel.findOne({ id: award_id });
     return award;
   }
+
+  static async update({ award_id, fieldToUpdate, newValue }) {
+    const filter = { id: award_id };
+    const update = { [fieldToUpdate]: newValue };
+    const option = { returnOriginal: false };
+
+    const updatedAward = await AwardModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return updatedAward;
+  }
 }
 
 export { Award };
