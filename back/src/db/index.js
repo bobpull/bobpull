6e2b0db1b042bc54ffa5d0920481bb519e5d82ad
@eikjae <eikjae@gmail.com>
2022-03-17 12:55:58 +0900

    merge 충돌 예방 작업

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
-import { Award } from "./models/Award";
+
+
+
 import { Certificate } from "./models/Certificate";
 
 const DB_URL =
@@ -17,4 +19,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Award, Certificate };
+export { User, Certificate };
