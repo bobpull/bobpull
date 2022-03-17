cc389531f14b4f5f4be2f572d5d78007196ae69a
jjssong <wkdthdrnt@naver.com>
2022-03-18 03:14:47 +0900

    목록 부분 모듈화

---
+++ front/src/components/project/ProjectCard.js
@@ -0,0 +1,48 @@
+import React, {useState} from "react"
+import {Button, Card, Col, Row} from "react-bootstrap";
+
+
+import ProjectEditForm from "./ProjectEditForm";
+
+
+const ProjectCard = ({item, projects, setProjects, isEditable, fetchAPI }) => {
+  const [isEditForm, setIsEditForm] = useState(false)
+  
+  return (
+    <Card className="mb-3" style={{textAlign: "left", border: "none"}} >
+      <Row>
+        {isEditForm ? 
+          <ProjectEditForm
+            item={item}
+            projects={projects}
+            setIsEditForm={setIsEditForm}
+            setProjects={setProjects}
+            fetchAPI={fetchAPI}
+          /> : 
+          <>
+            <Col sm="11">
+              <Card.Text className="mb-0" >
+                {item.title}
+              </Card.Text>
+              <Card.Text className=" mb-0 text-muted">
+                {item.description}
+              </Card.Text>
+              <Card.Text className="text-muted mb-2">
+                {item.from_date} ~ {item.to_date}
+              </Card.Text>
+            </Col>
+            <Col className="p-0 text-center">
+            { isEditable && <Button
+              variant="outline-info"
+              size="sm"
+              onClick={() => setIsEditForm(true)}
+            >편집</Button>}
+            </Col>
+          </>
+        }
+      </Row>
+    </Card>
+  )
+}
+
+export default ProjectCard
