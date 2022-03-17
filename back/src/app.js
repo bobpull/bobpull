83f80013a97045859f0cb62b0c0eebfa7274e261
@eikjae <eikjae@gmail.com>
2022-03-17 12:53:54 +0900

    merge 충돌 예방 작업

--- back/src/app.js
+++ back/src/app.js
@@ -1,7 +1,9 @@
 import cors from "cors";
 import express from "express";
 import { userAuthRouter } from "./routers/userRouter";
-import { userAwardRouter } from "./routers/awardRouter";
+
+
+
 import { userCertificateRouter } from "./routers/certificateRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
@@ -23,7 +25,9 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
-app.use(userAwardRouter);
+
+
+
 app.use(userCertificateRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
