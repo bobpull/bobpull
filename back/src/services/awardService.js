9d8a45d0c47bb32fd47c0359f6ddeab7f0003af1
@eikjae <eikjae@gmail.com>
2022-03-17 13:41:31 +0900

    create 중복 제거 기능 추가 과정

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -3,10 +3,15 @@ import { v4 as uuidv4 } from "uuid";
 
 class awardAuthService {
   static async addAward({ user_id, title, description }) {
-
-    // id 는 유니크 값 부여
-    const id = uuidv4();
-    const newAward = { id, user_id, title, description };
+    // 수상 이력 중복 확인
+    const titleWithDiscription = await Award.findByTitleWithDiscription({ title, description });
+    if (titleWithDiscription) {
+      const errorMessage =
+        "동일한 수상 이력을 중복으로 등록할 수 없습니다.";
+      return { errorMessage }
+    }
+    
+    const newAward = { user_id, title, description };
 
     // db에 저장
     const createdNewAward = await Award.create({ newAward });
