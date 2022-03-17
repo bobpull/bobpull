b18fc630e0e433371ad5f95c1ffb70ddca6323d4
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-17 11:37:04 +0900

    merge 준비

--- back/src/app.js
+++ back/src/app.js
@@ -2,6 +2,9 @@ import cors from "cors";
 import express from "express";
 import { userAuthRouter } from "./routers/userRouter";
 import { userEducationRouter } from "./routers/educationRouter";
+
+
+
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
@@ -23,6 +26,9 @@ app.get("/", (req, res) => {
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
 app.use(userEducationRouter);
+
+
+
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
