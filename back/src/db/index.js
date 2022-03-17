412b1dafebd7bace8f57ce7e3b9b6009edc2b960
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-17 11:37:04 +0900

    merge 준비

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import { User } from "./models/User";
 import { Education } from "./models/Education";
 
+
+
+
 const DB_URL =
   process.env.MONGODB_URL ||
   "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.";
