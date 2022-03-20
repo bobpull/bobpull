cfd6a964943904355c329c1d76548114a9d9ac79
kminzy <kminzy0704@gmail.com>
2022-03-20 23:59:01 +0900

    _id -> id 수정, add/put/delete 에러 발생시 alert

--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -32,6 +32,7 @@ function AwardAddForm({ setIsAdding }) {
       });
       setIsAdding(false);
     } catch (err) {
+      alert('동일한 수상 이력을 중복으로 등록할 수 없습니다.');
       console.error(err);
     }
   };
