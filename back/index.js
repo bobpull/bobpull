a18903c382eb4e9d3d34e93d24b0e89fb701989f
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-21 07:28:25 +0900

    db 연결 메시지 변경

--- back/index.js
+++ back/index.js
@@ -4,5 +4,5 @@ import { app } from "./src/app";
 const PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+  console.log(`재익아 오늘도 좋은 하루 보내(ง •̀_•́)ง  http://localhost:${PORT}`);
 });
