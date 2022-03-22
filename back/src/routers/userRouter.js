7beaa39ed2ba1321b1330e7e42138adb9656b873
@eikjae <eikjae@gmail.com>
2022-03-22 15:08:23 +0900

    이메일 변경 금지 추가

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -99,11 +99,10 @@ userAuthRouter.put(
       const user_id = req.params.id;
       // body data 로부터 업데이트할 사용자 정보를 추출함.
       const name = req.body.name ?? null;
-      const email = req.body.email ?? null;
       const password = req.body.password ?? null;
       const description = req.body.description ?? null;
 
-      const toUpdate = { name, email, password, description };
+      const toUpdate = { name, password, description };
 
       // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
       const updatedUser = await userAuthService.setUser({ user_id, toUpdate });
