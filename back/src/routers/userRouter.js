28be748d45da5c8f5775aa92c07b5d7842f05363
@eikjae <eikjae@gmail.com>
2022-03-23 14:39:24 +0900

    재가입 방지 관련 알림 추가

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -67,7 +67,13 @@ userAuthRouter.post("/user/reset-password", async function (req, res, next) {
 userAuthRouter.post("/user/verification-number", async function (req, res, next) {
   try {
     const email = req.body.email;
-    
+    const user = await User.findByEmail({ email });
+    if (user) {
+      const errorMessage =
+        "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.";
+      return { errorMessage };
+    }
+
     const verificationNumber = generateRandomPassword();
 
     await sendMail(email, `${verificationNumber}`)
