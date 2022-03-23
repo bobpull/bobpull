5cd89f0e8b265b4d5a55589ca9629a51d2dce833
jjssong <wkdthdrnt@naver.com>
2022-03-24 04:34:18 +0900

    유저정보 수정 코드 길이 축소

--- front/src/components/user/UserEditForm.js
+++ front/src/components/user/UserEditForm.js
@@ -1,62 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button, Form, Card, Col, Row } from "react-bootstrap";
 import * as Api from "../../api";
 
 function UserEditForm({ user, setIsEditing, setUser }) {
-  //useState로 name 상태를 생성함.
-  const [name, setName] = useState(user.name);
-  //useState로 email 상태를 생성함.
-  const [email, setEmail] = useState(user.email);
-  //useState로 description 상태를 생성함.
-  const [description, setDescription] = useState(user.description);
+  
+  const [info, setInfo] = useState({
+    name: user.name,
+    email: user.email,
+    description: user.description
+  })
+
+  const handleChange = (e) => {
+      setInfo(cur => {
+        return {
+          ...cur,
+          [e.target.name]: e.target.value
+        }
+      })
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // "users/유저id" 엔드포인트로 PUT 요청함.
-    const res = await Api.put(`users/${user.id}`, {
-      name,
-      email,
-      description,
-    });
-    // 유저 정보는 response의 data임.
-    const updatedUser = res.data;
-    // 해당 유저 정보로 user을 세팅함.
-    setUser(updatedUser);
-
-    // isEditing을 false로 세팅함.
-    setIsEditing(false);
+    try{
+      const res = await Api.put(`users/${user.id}`, info);
+      const updatedUser = res.data;
+  
+      setUser(updatedUser);
+      setIsEditing(false);
+    }catch(e){
+      console.log(e)
+    }
   };
 
   return (
     <Card className="mb-2">
       <Card.Body>
         <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="useEditprofile" className="mb-3">
+            <Form.Control
+              placeholder="사진"
+            />
+          </Form.Group>
           <Form.Group controlId="useEditName" className="mb-3">
             <Form.Control
               type="text"
               placeholder="이름"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={info.name}
+              name="name"
+              onChange={handleChange}
             />
           </Form.Group>
 
           <Form.Group controlId="userEditEmail" className="mb-3">
             <Form.Control
               type="email"
+              name="email"
               placeholder="이메일"
-              value={email}
+              value={info.email}
               disabled={true}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleChange}
             />
           </Form.Group>
 
           <Form.Group controlId="userEditDescription">
             <Form.Control
               type="text"
+              name="description"
               placeholder="정보, 인사말"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
+              value={info.description}
+              onChange={handleChange}
             />
           </Form.Group>
 
