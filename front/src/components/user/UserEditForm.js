246ee1cc8e3db396005f185b6b0ba9cb90cf984c
jjssong <wkdthdrnt@naver.com>
2022-03-24 06:22:16 +0900

    프로필 사진 변경 클라이언트에서만

--- front/src/components/user/UserEditForm.js
+++ front/src/components/user/UserEditForm.js
@@ -1,13 +1,20 @@
 import React, { useState, useRef } from "react";
 import { Button, Form, Card, Col, Row } from "react-bootstrap";
 import * as Api from "../../api";
+import "../../style/display.css"
 
 function UserEditForm({ user, setIsEditing, setUser }) {
-  
+
+  const imgRef = useRef()
   const [info, setInfo] = useState({
     name: user.name,
     email: user.email,
     description: user.description
+  })
+  const [image, setImage] = useState({
+    id: "",
+    file: "",
+    url: "http://placekitten.com/200/200"
   })
 
   const handleChange = (e) => {
@@ -32,24 +39,50 @@ function UserEditForm({ user, setIsEditing, setUser }) {
     }
   };
 
+  const handleProfile = (e) => {
+    const newImg = e.target.files
+    setImage(cur => {
+      return{
+        id: newImg[0].name, 
+        file: newImg[0], 
+        url: URL.createObjectURL(newImg[0])}
+    })
+   console.log(image)
+    
+    
+  }
+
   return (
     <Card className="mb-2">
       <Card.Body>
         <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="useEditprofile" className="mb-3">
-            <Form.Control
-              placeholder="사진"
-            />
-          </Form.Group>
-          <Form.Group controlId="useEditName" className="mb-3">
-            <Form.Control
-              type="text"
-              placeholder="이름"
-              value={info.name}
-              name="name"
-              onChange={handleChange}
-            />
-          </Form.Group>
+        <Card.Img
+          ref={imgRef}
+          style={{ width: "50px", height: "50px", borderRadius: "50%", marginRight: "10px", cursor: "pointer" }}
+          src={image.url}
+          alt="프로필 사진"
+          onClick={() => {imgRef.current.click()}}
+        />
+        <Form.Group controlId="useEditprofile" className="mb-3 ">
+        <Form.Control
+          style={{display: "none"}}
+          type="file"            
+          name="image"
+          ref={imgRef}
+          accept=""
+          onChange={handleProfile}
+        />
+       
+        </Form.Group>
+        <Form.Group controlId="useEditName" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="이름"
+            value={info.name}
+            name="name"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
           <Form.Group controlId="userEditEmail" className="mb-3">
             <Form.Control
