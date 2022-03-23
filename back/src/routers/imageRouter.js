23a20fd71ed313ce1fd64d70c9d4fb222542c924
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-24 06:03:36 +0900

    image API 생성중

--- back/src/routers/imageRouter.js
+++ back/src/routers/imageRouter.js
@@ -1,24 +1,54 @@
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
-import { upload, delImg } from "../middlewares/imageMiddleware";
+import { upload, nameField } from "../middlewares/multerMiddleware";
+import { ImageService } from "../services/imageService";
 
 const ImageRouter = Router();
 
 ImageRouter.post(
-  "/upload", 
+  '/profile', 
   login_required, 
-  upload.single('filename'), 
-  delImg, 
-  async function (req, res) {
-    const profile = req.file.location;
-    res.status(201).send(profile);
+  upload.single(nameField), 
+  async function (req, res, next) {
+    try{
+      const filePath = req.file.file_path;
+      const imageBuffer = fs.readFileSync(filePath); 
+      const contentType = req.file.mimetype;
+      const image = { 
+        data: imageBuffer,
+        contentType,
+      }
+      const user_id = req.currentUserId;
+      
+      const updatedImage = await ImageService.setUserImage({ user_id, image, filePath });
+  
+      if(updatedImage.errorMessage){
+        throw new Error(updatedImage.errorMessage);
+      }
+      
+      res.status(200).send(updatedImage);
+    } catch(error) {
+      next(error);
+    }
 });
+  
+ImageRouter.get(
+  "/profile", 
+  login_required, 
+  async function (req, res, next){
+    try{
+      const currentUserId = req.currentUserId;
+      const userImage = await ImageService.getUserImage({ currentUserId });
 
-ImageRouter.delete(
-  "/delete/:id", 
-  delImg, 
-  async function (req, res) {
-    res.status(204).send("delete_success");
-})
+      if(userImage.errorMessage){
+        throw new Error(userImage.errorMessage);
+      }
+    
+      res.status(200).send(userImage);
+    } catch(error) {
+      next(error);
+    }
+  }
+);
 
 export { ImageRouter };
