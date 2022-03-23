9c0b829cdb8f406077d894284c96ab41ebc8fa31
jjssong <wkdthdrnt@naver.com>
2022-03-23 15:33:48 +0900

    inline css을 css파일로 수정

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Card, Row, Button, Col } from "react-bootstrap";
+import "../../style/display.css"
+import "../../style/font.css"
 
 import { AiFillRead } from "react-icons/ai";
 
@@ -11,33 +13,23 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork }) {
     <Card className="mb-2 ms-0" style={{ width: "100%", margin: "0 auto" }}>
       <Card.Body>
         <Row>
-          <div style={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}>
+          <div className="between">
             <div style={{
               display: "flex",
             }}>
-            <Card.Img
-              style={{ width: "50px", height: "50px", borderRadius: "50%", marginRight: "10px" }}
-              className="mb-3"
-              src="http://placekitten.com/200/200"
-              alt="고양이 사진"
-            />
-            <div>
-              <Card.Title style={{
-                fontSize: "1rem",
-              }}>{user?.name}</Card.Title>
-              <Card.Subtitle style={{
-                fontSize: "0.9rem"
-              }} className="text-muted">{user?.email}</Card.Subtitle>
-              <Card.Text
+              <Card.Img
+                style={{ width: "50px", height: "50px", borderRadius: "50%", marginRight: "10px" }}
                 className="mb-3"
-               style={{
-                fontSize: "0.9rem",
-              }}
-              >{user?.description}</Card.Text>
-            </div>
+                src="http://placekitten.com/200/200"
+                alt="고양이 사진"
+              />
+              <div>
+                <Card.Title className="title">{user?.name}</Card.Title>
+                <Card.Subtitle className="text-muted text">{user?.email}</Card.Subtitle>
+                <Card.Text
+                  className="mb-3 text"
+                >{user?.description}</Card.Text>
+              </div>
             </div>
             {isNetwork && (
               <Card.Link
