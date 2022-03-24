5d5873438539fe6312fe3e16971226dfe446b8c5
eunna <dladmssk98@gmail.com>
2022-03-25 06:04:58 +0900

    ProfileProvider 오류 발생으로 인한 주석 처리

--- front/src/index.js
+++ front/src/index.js
@@ -4,16 +4,16 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 
-import {ProfileProvider} from "./context/ProfileContext"
+// import { ProfileProvider } from "./context/ProfileContext";
 import { FriendListProvider } from "./context/FriendListContext";
 
 ReactDOM.render(
   <React.StrictMode>
-      <ProfileProvider>
-        <FriendListProvider>
-          <App />
-        </FriendListProvider>
-      </ProfileProvider>
+    {/* <ProfileProvider> */}
+    <FriendListProvider>
+      <App />
+    </FriendListProvider>
+    {/* </ProfileProvider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
