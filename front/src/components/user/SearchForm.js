0fd8439f029349f96d79d9a0dd2a81cfc0704d7e
eunna <dladmssk98@gmail.com>
2022-03-25 16:10:09 +0900

    form 입력 타입 수정

--- front/src/components/user/SearchForm.js
+++ front/src/components/user/SearchForm.js
@@ -5,7 +5,7 @@ function SearchForm() {
   return (
     <Form.Group className="between">
       <Form.Control
-        type="password"
+        type="text"
         autoComplete="off"
         placeholder="검색할 사용자의 이름을 입력하세요."
         style={{
