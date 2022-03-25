b745e227701266602c89133c2c83bdf99177f7de
jjssong <wkdthdrnt@naver.com>
2022-03-25 14:34:40 +0900

    context provider 수정

--- front/src/index.js
+++ front/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
-
+import { ProjectProvider } from "./context/ProjectContext";
 import {ProfileProvider} from "./context/ProfileContext"
 import { FriendListProvider } from "./context/FriendListContext";
 
@@ -11,7 +11,9 @@ ReactDOM.render(
   <React.StrictMode>
       <ProfileProvider>
         <FriendListProvider>
+        <ProjectProvider>
           <App />
+          </ProjectProvider>
         </FriendListProvider>
       </ProfileProvider>
   </React.StrictMode>,
