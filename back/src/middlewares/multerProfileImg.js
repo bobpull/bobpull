59e6aa7ac55b6d797c82a9262b53f9fc24089f15
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-03-27 00:02:06 +0900

    필요없는 거 삭제

--- back/src/middlewares/multerProfileImg.js
+++ back/src/middlewares/multerProfileImg.js
@@ -2,14 +2,15 @@ import multer from "multer"
 import path from "path"
 
 const upload = multer({
-  storage: multer.diskStorage({  // 이미지 저장 정보 설정
-    destination(req, file, done) { 
-      done(null, "uploads/profile_img"); 
+  storage: multer.diskStorage({  
+    destination(req, file, cb) { 
+      cb(null, 'uploads/profile_img'); 
     },
-    filename(req, file, done) {   // 이미지 이름 설정
+    
+    filename(req, file, cb) {   
       const ext = path.extname(file.originalname); 
       const user_id = req.params.id
-      done(null, user_id + "_" + Date.now() + ext); // userId + 날짜 + 확장자 이름으로 저장
+      cb(null, user_id + "_" + Date.now() + ext);
     }
   }),
 });
