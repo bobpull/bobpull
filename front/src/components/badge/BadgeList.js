71f0f0e23f94c1ca6ab367ae5133b8e736c7f0a2
eunna <dladmssk98@gmail.com>
2022-03-27 00:18:14 +0900

    임시 커밋

---
+++ front/src/components/badge/BadgeList.js
@@ -0,0 +1,90 @@
+export const imgUrl = [
+  {
+    url: `${process.env.PUBLIC_URL}/img/pull.png`,
+    name: "pull",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/bowl.png`,
+    name: "bowl",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/egg.png`,
+    name: "egg",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/spam.png`,
+    name: "spam",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/cleanCode.png`,
+    name: "cleanCode",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/css3.png`,
+    name: "css3",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/dj.png`,
+    name: "dj",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/es6.png`,
+    name: "es6",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/GraphQL.png`,
+    name: "Graph_QL",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/grid.png`,
+    name: "grid",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/html5.png`,
+    name: "html5",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/JS.png`,
+    name: "JS",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/MongoDB.png`,
+    name: "MongoDB",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/NestJS.png`,
+    name: "NestJS",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/nodejs.png`,
+    name: "nodejs",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/Pug.png`,
+    name: "Pug",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/python.png`,
+    name: "python",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/ReactNative.png`,
+    name: "ReactNative",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/React.png`,
+    name: "React",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/socketio.png`,
+    name: "socketio",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/typescript.png`,
+    name: "typescript",
+  },
+  {
+    url: `${process.env.PUBLIC_URL}/img/websockets.png`,
+    name: "websockets",
+  },
+];
