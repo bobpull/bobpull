b9c7acc6e614ebc0bfc9387ded6b5110d9be555e
eunna <dladmssk98@gmail.com>
2022-03-26 15:22:54 +0900

    Badge 구매 시 톨 동기화 추가

--- front/src/components/badge/Badge.js
+++ front/src/components/badge/Badge.js
@@ -1,9 +1,11 @@
-import React, {useState, useEffect, useRef} from "react"
-import {Card, Row, Col, Badge} from "react-bootstrap";
-import * as Api from "../../api.js"
-import "../../style/display.css"
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { Card, Row, Col, Badge } from "react-bootstrap";
+import { setDefaultLocale } from "react-datepicker";
+import * as Api from "../../api.js";
+import "../../style/display.css";
 
-import BadgeLock from "./BadgeLock"
+import BadgeLock from "./BadgeLock";
+import { TallContext } from "../../context/TallContext";
 
 // const badgeName = ["pull", "bowl", "egg", "spam", "cleanCode", "css3", "dj", "es6", "GraphQL", "grid", "html5", "JS", "MongoDB", "NestJS", "nodejs", "Pug", "python", "ReactNative", "React", "socketio", "typescript", "websockets"];
 
@@ -15,126 +17,126 @@ const imgUrl = [
   {
     url: `${process.env.PUBLIC_URL}/img/pull.png`,
     name: "pull",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/bowl.png`,
     name: "bowl",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/egg.png`,
     name: "egg",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/spam.png`,
     name: "spam",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/cleanCode.png`,
     name: "cleanCode",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/css3.png`,
     name: "css3",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/dj.png`,
     name: "dj",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/es6.png`,
     name: "es6",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/GraphQL.png`,
     name: "Graph_QL",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/grid.png`,
     name: "grid",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/html5.png`,
     name: "html5",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/JS.png`,
     name: "JS",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/MongoDB.png`,
     name: "MongoDB",
-    isOpen: true
+    isOpen: true,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/NestJS.png`,
     name: "Nest_JS",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/nodejs.png`,
     name: "nodejs",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/Pug.png`,
     name: "Pug",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/python.png`,
     name: "python",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/ReactNative.png`,
     name: "clean-React_Native",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/React.png`,
     name: "React",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/socketio.png`,
     name: "socketio",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/typescript.png`,
     name: "typescript",
-    isOpen: false
+    isOpen: false,
   },
   {
     url: `${process.env.PUBLIC_URL}/img/websockets.png`,
     name: "websockets",
-    isOpen: false
+    isOpen: false,
   },
-]
-const Skill = ({portfolioOwnerId, isEditable}) => {
-
-  const imgRef = useRef()
+];
+const Skill = ({ portfolioOwnerId, isEditable }) => {
+  const imgRef = useRef();
   const [badges, setBadges] = useState([]);
+  const { setTall } = useContext(TallContext);
   useEffect(() => {
     const fetchAPI = async () => {
-      const res = await Api.get("badgelist", portfolioOwnerId)
-      console.log(res.data)
-      setBadges(res.data.map(item => item.id))
-    }
-    fetchAPI()
-  }, [portfolioOwnerId])
+      const res = await Api.get("badgelist", portfolioOwnerId);
+      console.log(res.data);
+      setBadges(res.data.map((item) => item.id));
+    };
+    fetchAPI();
+  }, [portfolioOwnerId]);
   /*
   BadgeRouter.post("/badge/:id",
   BadgeRouter.get("/badges/:id",
@@ -143,46 +145,45 @@ const Skill = ({portfolioOwnerId, isEditable}) => {
 
   const openBadge = async (index) => {
     try {
-       await Api.post(`badge/${index}`)
-      setBadges(cur => {
-          return [
-            ...cur,
-            index
-          ]
-      })
-    }catch(e){
-      
-    }
-  }
+      const res = await Api.post(`badge/${index}`);
+      setBadges((cur) => {
+        return [...cur, index];
+      });
+      setTall(res.data[1]);
+    } catch (e) {}
+  };
 
-  return <Card className="mb-3">
-    <Card.Body>
-      <Row xs="4" sm="5" md="6" lg="5" className="jusify-content-center">
-        {imgUrl.map((bedge, index) => (
-          <Col className="mb-3 col-center colBox"  onClick={openBadge}>
-            <Card.Img
-              key={index}
-              className={badges.includes(index.toString()) ? "cardImg spinAni" : "cardImg opacity"}
-              src={bedge.url}
-              ref={imgRef}
-              onClick={() => {
-                if(isEditable && !badges.includes(index.toString())){
-                  openBadge(index.toString())
+  return (
+    <Card className="mb-3">
+      <Card.Body>
+        <Row xs="4" sm="5" md="6" lg="5" className="jusify-content-center">
+          {imgUrl.map((bedge, index) => (
+            <Col className="mb-3 col-center colBox" onClick={openBadge}>
+              <Card.Img
+                key={index}
+                className={
+                  badges.includes(index.toString())
+                    ? "cardImg spinAni"
+                    : "cardImg opacity"
                 }
-                else return
-              }}
-            />
-            {!badges.includes(index.toString()) && <BadgeLock/>}
-            <Badge pill bg="light" text="dark">
-              {bedge.name}
-            </Badge>
-          </Col>
-          
-        ))}
-      </Row>
-      
-    </Card.Body>
-  </Card>
-}
+                src={bedge.url}
+                ref={imgRef}
+                onClick={() => {
+                  if (isEditable && !badges.includes(index.toString())) {
+                    openBadge(index.toString());
+                  } else return;
+                }}
+              />
+              {!badges.includes(index.toString()) && <BadgeLock />}
+              <Badge pill bg="light" text="dark">
+                {bedge.name}
+              </Badge>
+            </Col>
+          ))}
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default Skill
+export default Skill;
