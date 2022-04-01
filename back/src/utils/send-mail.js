a9028c8d17a3caa7cb30da7ff5bcca185434b371
@eikjae <eikjae@gmail.com>
2022-04-01 12:46:36 +0900

    nodemail 관련 .env 처리

--- back/src/utils/send-mail.js
+++ back/src/utils/send-mail.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 const transport = nodemailer.createTransport({
   service: "Gmail",
   auth: {
-    user: "llndqoq@gmail.com",
-    pass: "ipklsasvhxknmwne",
+    user: NODEMAILER_ID,
+    pass: NODEMAILER_PW,
   },
 });
 
