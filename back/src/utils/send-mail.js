9340c1e21662c83a2296cf9708fb952dfab1f7ed
@eikjae <eikjae@gmail.com>
2022-04-01 13:25:22 +0900

    nodemailer 임시방편 처리

--- back/src/utils/send-mail.js
+++ back/src/utils/send-mail.js
@@ -1,11 +1,12 @@
 const nodemailer = require("nodemailer");
+import 'dotenv/config'
 
 // nodemailer로 gmail transport 생성하기
 const transport = nodemailer.createTransport({
   service: "Gmail",
   auth: {
-    user: NODEMAILER_ID,
-    pass: NODEMAILER_PW,
+    user: process.env.NODEMAILER_ID2,
+    pass: process.env.NODEMAILER_PW2,
   },
 });
 
